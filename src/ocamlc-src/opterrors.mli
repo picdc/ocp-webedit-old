9d2de67d548dd5dec772df9233e1985ae8fa6ff4
david-maison <david.maison@hotmail.fr>
2013-07-12 13:01:55 +0200

    Debut de l'implémentation d'ocamlc en javascript

---
+++ src/ocamlc-src/opterrors.mli
@@ -0,0 +1,17 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id: opterrors.mli 11156 2011-07-27 14:17:02Z doligez $ *)
+
+(* Error report *)
+
+val report_error: Format.formatter -> exn -> unit
---
+++ src/ocamlc-src/optmain.mli
@@ -0,0 +1,17 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*          Damien Doligez, projet Moscova, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 2000 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id: optmain.mli 11156 2011-07-27 14:17:02Z doligez $ *)
+
+(*
+  this "empty" file is here to speed up garbage collection in ocamlopt.opt
+*)
