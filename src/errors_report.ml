3dc862b46e266e811a7c0575a52a53f6b3c0cb9d
Pierrick <pierrick.couderc@u-psud.fr>
2013-07-31 16:05:03 +0200

    Bugfix for error report

--- src/errors_report.ml
+++ src/errors_report.ml
@@ -4,7 +4,22 @@ open Errors_format
 let make_error_report project error =
   (* let id = Filemanager.get_id ~project ~filename:error.file in *)
   Dom_html.handler (fun _ ->
-    Eventmanager.go_to_next_error#trigger (project, error);
+    let id = Filemanager.get_id ~project ~filename:error.file in
+    if Filemanager.is_file_opened ~project ~filename:error.file then
+      begin
+        Eventmanager.switch_file#trigger id;
+        let e = Global.editor () in
+        let r = Ace.range 
+          (error.line -1) (fst error.chars)
+          (error.line -1) (snd error.chars)
+        in
+        if fst error.chars >= 0 then
+          e##getSelection()##setSelectionRange(r, Js._false)
+        else (e##moveCursorTo((error.line - 1), 0);
+              e##getSelection()##selectLine())
+      end
+    else
+      Eventmanager.go_to_next_error#trigger (project, error);
     Js._true)
     
 let add_errors_reports project errors =
